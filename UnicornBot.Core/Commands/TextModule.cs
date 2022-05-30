c7ce8a59856eff63f225bede22b816c171b6d9d1
David Langr <david.langr@unicornuniversity.net>
2022-05-30 12:17:32 +0200

    Add the ping command

---
+++ UnicornBot.Core/Commands/TextModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+
+namespace UnicornBot.Core.Commands
+{
+    public class TextModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        [Summary("Pings the bot")]
+        public Task PingAsync()
+        {
+            return ReplyAsync($"Pong, {Context.Message.Author.Mention}!");
+        }
+    }
+}
