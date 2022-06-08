da37326e43793afc153e8c8e03ffda7dcf6c5652
David Langr <david.langr@unicornuniversity.net>
2022-06-08 11:44:51 +0200

    Move the event handlers to a separate class

--- UnicornBot.Core/Bot.cs
+++ UnicornBot.Core/Bot.cs
@@ -1,6 +1,5 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
-using DSharpPlus.EventArgs;
 using Microsoft.Extensions.Logging;
 using System.Reflection;
 using UnicornBot.Core.Model;
@@ -12,7 +11,6 @@ public class Bot
     public DiscordClient Client { get; private set; }
     public CommandsNextExtension Commands { get; private set; }
     private readonly Config _config;
-    private readonly EventId _botEventId = new(420, "UnicornBot");
 
     public Bot(Config config)
     {
@@ -22,9 +20,9 @@ public class Bot
 
         Commands = RegisterCommands().Result;
 
-        Client.Ready += Client_Ready;
-        Client.GuildAvailable += Client_GuildAvailable;
-        Client.ClientErrored += Client_ClientErrored;
+        Client.Ready += EventHandlers.Client_Ready;
+        Client.GuildAvailable += EventHandlers.Client_GuildAvailable;
+        Client.ClientErrored += EventHandlers.Client_ClientErrored;
     }
 
     public async Task ConnectAsync()
@@ -63,38 +61,8 @@ public class Bot
 
         commands.RegisterCommands(Assembly.GetExecutingAssembly());
 
-        commands.CommandErrored += Commands_CommandErrored;
+        commands.CommandErrored += EventHandlers.Commands_CommandErrored;
 
         return Task.FromResult(commands);
-    }
-
-    private Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
-    {
-        sender.Logger.LogInformation(_botEventId, "Bot started.");
-
-        return Task.CompletedTask;
-    }
-
-    private Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
-    {
-        sender.Logger.LogInformation(_botEventId, "Connected to '{Guild Name}'.", e.Guild.Name);
-
-        return Task.CompletedTask;
-    }
-
-    private Task Client_ClientErrored(DiscordClient sender, ClientErrorEventArgs e)
-    {
-        sender.Logger.LogError(_botEventId, e.Exception, "An exception has occured.");
-
-        return Task.CompletedTask;
-    }
-
-    private Task Commands_CommandErrored(CommandsNextExtension sender, CommandErrorEventArgs e)
-    {
-        Client.Logger.LogTrace(_botEventId, e.Exception, "A command error has occured.");
-
-        e.Context.RespondAsync("Permission denied.");
-
-        return Task.CompletedTask;
     }
 }
---
+++ UnicornBot.Core/Helpers/EventHandlers.cs
@@ -0,0 +1,52 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.CommandsNext.Exceptions;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+
+namespace UnicornBot.Core;
+
+internal class EventHandlers
+{
+
+    private static readonly EventId _botEventId = new(420, "UnicornBot");
+
+    internal static Task Client_Ready(DiscordClient sender, ReadyEventArgs e)
+    {
+        sender.Logger.LogInformation(_botEventId, "Bot started.");
+
+        return Task.CompletedTask;
+    }
+
+    internal static Task Client_GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+    {
+        sender.Logger.LogInformation(_botEventId, "Connected to '{Guild Name}'.", e.Guild.Name);
+
+        return Task.CompletedTask;
+    }
+
+    internal static Task Client_ClientErrored(DiscordClient sender, ClientErrorEventArgs e)
+    {
+        sender.Logger.LogError(_botEventId, e.Exception, "An exception has occured.");
+
+        return Task.CompletedTask;
+    }
+
+    internal static async Task Commands_CommandErrored(CommandsNextExtension sender, CommandErrorEventArgs e)
+    {
+        sender.Client.Logger.LogTrace(_botEventId, e.Exception, "A command error has occured.");
+
+        IReadOnlyList<CheckBaseAttribute> failedChecks = ((ChecksFailedException)e.Exception).FailedChecks;
+
+        foreach (CheckBaseAttribute failedCheck in failedChecks)
+        {
+            string msg = failedCheck switch
+            {
+                _ => $"An unknown error has occured:\r\n{e.Exception}",
+            };
+
+            await e.Context.RespondAsync(msg).ConfigureAwait(false);
+        }
+    }
+}
